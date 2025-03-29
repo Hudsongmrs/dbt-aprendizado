e1ca436ae5b5b8b7ca400a939c9cd00cecad262c
Nimrais A <contanimrais@gmail.com>
2025-03-29 16:41:39 +0000

    tabela_filha_2

--- models/staging/stg_tabela_filha.sql
+++ models/staging/stg_tabela_filha.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
 
 SELECT * 
-FROM `teste-etl-2.dados.tabela_filha`  
+FROM `teste-etl-2.dados.tabela_filha_2`  
 WHERE year > 2018 AND industry_code_ANZSIC NOT IN ('A', 'D')
